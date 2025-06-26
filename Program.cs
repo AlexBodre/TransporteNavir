f863c4fda60c9ebbcfdb77c301bd087f1452182d
alex <aboordonez@gmail.com>
2025-06-26 19:42:43 -0400

    landing page changes

--- Program.cs
+++ Program.cs
@@ -5,10 +5,14 @@ using TransporteNavir.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// ...existing code...
+
+
+// ...existing code...
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddBootstrapBlazor(); // <-- Add this line
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
